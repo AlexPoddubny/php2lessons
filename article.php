0d9b6033dde672d529a8a865071c538a8d8c3444
AlexPoddubny <usatij.alex@gmail.com>
2019-11-06 01:52:30 +0200

    + работа с Article

--- article.php
+++ article.php
@@ -1,4 +1,6 @@
 <?php
 	
+	require __DIR__ . '/autoload.php';
+	$article = \App\Models\News::findById($_GET['id']);
+	var_dump($article);
 	include __DIR__ . '/App/templates/article.php';
-	$article = \App\Models\News::findById($id);
--- index.php
+++ index.php
@@ -5,8 +5,11 @@
 	//$db = new \App\Db();
 	//echo \App\Models\User::$table;
 	//$users = \App\Models\User::findById(2);
+	//$users = \App\Models\User::findAll();
 	//$users = \App\Models\User::getByCount(2);
 	$news = \App\Models\News::getByCount(3);
+	//$news = \App\Models\News::findAll();
+	var_dump($news);
 	
 	include __DIR__ . '/App/templates/news.php';
 	
