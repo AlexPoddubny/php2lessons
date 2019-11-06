e83af97dc829e39785e786e3e50cf02105532b55
AlexPoddubny <usatij.alex@gmail.com>
2019-11-06 14:39:20 +0200

    + ActiveRecord insert()

--- App/Model.php
+++ App/Model.php
@@ -8,6 +8,8 @@
 	{
 		const TABLE = '';
 		
+		public $id;
+		
 		public static function findAll($param = [])
 		{
 			$db = Db::instance();
@@ -26,6 +28,32 @@
 				static::class,
 				$param
 			);
+		}
+		
+		public function isNew()
+		{
+			return empty($this->id);
+		}
+		
+		public function insert()
+		{
+			if (!$this->isNew()){
+				return;
+			}
+			$columns = [];
+			$values = [];
+			foreach ($this as $k => $v){
+				if ('id' == $k){
+					continue;
+				}
+				$columns[] = $k;
+				$values[':' . $k] = $v;
+			}
+			$sql = 'INSERT INTO ' . static::TABLE . ' (' .
+				implode(',', $columns) . ')
+				VALUE (' . implode(',', array_keys($values)) . ')';
+			$db = Db::instance();
+			$db->execute($sql, $values);
 		}
 		
 		public static function getByCount(Int $count)
--- index.php
+++ index.php
@@ -1,24 +1,11 @@
 <?php
 	
-	use App\Model;
-	use App\Models\HasEmail;
 	use App\Models\User;
 	
 	require __DIR__ . '/autoload.php';
-	
-	//$db = new \App\Db();
-	//echo \App\Models\User::$table;
-	//$users = \App\Models\User::findById(2);
-	$users = User::findAll();
-	//$users = \App\Models\User::getByCount(2);
-	//$news = \App\Models\News::getByCount(3);
-	//$news = \App\Models\News::findAll();
-	//var_dump($users[0]->getEmail());
-	function sendEmail(HasEmail $user, string $message)
-	{
-		echo 'Sending ' . $message . ' to ' . $user->email;
-	}
-	sendEmail($users[0], 'Yo, man!');
-	
-	//include __DIR__ . '/App/templates/news.php';
+
+	$user = new User();
+	$user->name = 'Vasya';
+	$user->email = 'test@test.com';
+	$user->insert();
 	
