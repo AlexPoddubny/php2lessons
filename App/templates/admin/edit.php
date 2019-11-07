4d84dae8b9bb8f175d1cb1653a0ccbf156af0c64
AlexPoddubny <usatij.alex@gmail.com>
2019-11-07 23:17:29 +0200

    +корректировка редакции новости

--- App/templates/admin/edit.php
+++ App/templates/admin/edit.php
@@ -31,17 +31,13 @@
 		?>новость</h1>
 	<form action="/App/Admin/edit.php" method="post">
 		<div>
-			<input type="text" name="title" style="width: 100%; text-align: left" value="
-				<?php echo trim($article->title); ?>
-			">
+			<input type="text" name="title" style="width: 100%; text-align: left" value="<?php echo trim($article->title); ?>">
 			<?php if ($article->id){ ?>
 				<input type="hidden" name="id" value="<?php echo $article->id; ?>">
 			<?php } ?>
 		</div>
 		<div>
-			<textarea name="content" style="width: 100%; height: 500px">
-				<?php echo trim($article->content);?>
-			</textarea>
+			<textarea name="content" style="width: 100%; height: 500px"><?php echo trim($article->content);?></textarea>
 		</div>
 		<input type="submit" value="Сохранить">
 	</form>
