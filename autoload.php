e67216d03b3ab8e41b7acacb65e6b445f9d074ca
AlexPoddubny <usatij.alex@gmail.com>
2019-11-15 13:26:38 +0200

    +spl_autoload lib

--- autoload.php
+++ autoload.php
@@ -4,9 +4,17 @@ function autoloader($class)
 {
 	$file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
 	if (file_exists($file)){
-		require $file;
+		include $file;
 	} return false;
 	
 }
 
 spl_autoload_register('autoloader');
+
+spl_autoload_register(function ($class){
+	$file = __DIR__ . '/' . str_replace(['\\', 'App'], ['/', 'lib'], $class) .
+		'.php';
+	if (file_exists($file)){
+		include $file;
+	} return false;
+});
