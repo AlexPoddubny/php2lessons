33b7c28dc4186a34ab7667c148752a0e988ec7be
AlexPoddubny <usatij.alex@gmail.com>
2019-11-18 17:34:18 +0200

    +замыкания

--- test.php
+++ test.php
@@ -1,12 +1,9 @@
 <?php
 	
-	use App\Collection;
-	
-	require __DIR__ . '/autoload.php';
-	$a = new Collection;
-	$a[1] = 1;
-	$a[11] = 11;
-	$a[2] = 22;
-	foreach ($a as $el){
-		echo $el;
-	}
+	$prefix = 'project';
+	$f = function ($name) use ($prefix){
+		return $prefix . '_' . $name;
+	};
+	echo $f('users');
+	$prefix = 'table';
+	echo $f('users');
